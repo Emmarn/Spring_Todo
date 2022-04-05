c2df5e42713e38c9abc884236573eb48fc5f2114
Emmarn <92029409+Emmarn@users.noreply.github.com>
2022-04-05 15:49:19 +0200

    Merge branch 'master' of https://github.com/Emmarn/Spring_Todo

--- src/main/java/com/example/spring_todo/SpringTodoApplication.java
+++ src/main/java/com/example/spring_todo/SpringTodoApplication.java
@@ -1,7 +1,11 @@
 package com.example.spring_todo;
 
+import com.example.spring_todo.Repositorys.TodoRepository;
+import com.example.spring_todo.entities.Todo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringTodoApplication {
@@ -9,5 +13,14 @@ public class SpringTodoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringTodoApplication.class, args);
     }
+
+   /* @Bean
+
+    CommandLineRunner init(TodoRepository todoRepository){
+        return args->{
+            Todo todo=new Todo("Städa", "Städa i badrummet");
+            todoRepository.save(todo);
+        };
+    }*/
 
 }
